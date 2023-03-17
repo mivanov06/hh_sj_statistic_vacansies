e953d0359046943862e61484bf2cab594e5f1ae9
mivanov06 <mivanov06@yandex.ru>
2023-03-17 15:49:09 +1000

    fix main.py

--- main.py
+++ main.py
@@ -2,19 +2,17 @@ import os
 from dotenv import load_dotenv
 
 from hh_statistic import hh_get_vacancy_statistic
-from job_statistic_func import print_table
 from sj_statistic import sj_get_vacancy_statistic
+from job_statistic_func import print_table
 
 
 if __name__ == "__main__":
     load_dotenv()
-    languages = ['Python', 'Java', 'C++', 'C#', 'C',
-                 'PHP', 'Go', 'Ruby', 'JavaScript', 'TypeScript']
+    languages = ['Python', 'Java', 'C++', 'C#', 'C', 'PHP', 'Go', 'Ruby', 'JavaScript', 'TypeScript']
     sj_key = os.getenv('SJ_SECRET_KEY')
     hh_salary_ranges, sj_salary_ranges = [], []
     table_lines = [
-        ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано',
-         'Средняя зарплата']
+        ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
     ]
     for language in languages:
         hh_salary_ranges.append(hh_get_vacancy_statistic(language))
